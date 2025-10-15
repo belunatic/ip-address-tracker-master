efe0f79ec36b8358ef08840380fb5c22b4187b5e
belunatic <abelluvyou@yahoo.com>
2025-10-15 14:59:47 -0700

    js folder added

---
+++ js/index.js
@@ -0,0 +1,10 @@
+import { API_KEY } from "./secret";
+//get DOM elements
+const form = document.getElementById("ip-form");
+const ipInput = document.getElementById("ip-address");
+//result DOM
+const ipAddress = document.getElementById("ip-address");
+const city = document.getElementById("city");
+const zipCode = document.getElementById("zip-code");
+const timezone = document.getElementById("timezone");
+const isp = document.getElementById("isp");
---
+++ js/secret.js
@@ -0,0 +1,3 @@
+export const API_KEY = "at_psJuirwEUC451grx6GsU8YWzb7QeD";
+
+//https://geo.ipify.org/api/v2/country,city?apiKey=at_psJuirwEUC451grx6GsU8YWzb7QeD&ipAddress=8.8.8.8
