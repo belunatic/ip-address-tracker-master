2773ca7303faffb74b8de38635496ef5e4e28ae5
belunatic <abelluvyou@yahoo.com>
2025-10-15 15:30:48 -0700

    fetch data from API ad display result in DOM

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import { API_KEY } from "./secret";
+import { API_KEY } from "./secret.js";
 //get DOM elements
 const form = document.getElementById("ip-form");
 const ipInput = document.getElementById("ip-address");
@@ -8,3 +8,37 @@ const city = document.getElementById("city");
 const zipCode = document.getElementById("zip-code");
 const timezone = document.getElementById("timezone");
 const isp = document.getElementById("isp");
+
+//DOM loaded
+//document.addEventListener("DOMContentLoaded", apiFetch());
+
+async function apiFetch(ip = "") {
+	try {
+		const res = await fetch(
+			`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}`
+		);
+
+		if (!res.ok) {
+			throw new Error(`Fetching error, status -> ${res.status}`);
+		}
+		//get the data
+		const data = await res.json();
+		displayResult(data);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+//display result in DOM
+function displayResult(data) {
+	ipAddress.textContent = data.ip;
+	city.textContent = `${data.location.city}, ${data.location.region}`;
+	timezone.textContent = `UTC ${data.location.timezone}`;
+	isp.textContent = data.isp;
+}
+
+//form submission
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	apiFetch(ipInput.value);
+});
